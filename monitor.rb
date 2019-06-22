f9b2e1fad962626a1c487168cda75c09223881c1
C.Viktor <vchekryzhov@ya.ru>
2019-06-22 16:12:34 +0300

    limit

--- monitor.rb
+++ monitor.rb
@@ -9,7 +9,7 @@ Telegram::Bot::Client.run(token) do |bot|
   while 1
     filesystem_stat = Sys::Filesystem.stat('/')
     free_gb = (filesystem_stat.block_size * filesystem_stat.blocks_available) / 1024 / 1024 / 1024
-    bot.api.send_message(chat_id: "@state_monitor", text: "Warning free disck space #{free_gb} GB") if free_gb < 129
+    bot.api.send_message(chat_id: "@state_monitor", text: "Warning free disck space #{free_gb} GB") if free_gb < 3
     sleep 5
   end
 end
